b7f61a57be7e3d0874456b658415ff2e16a7d031
tihuan <TimmyHuang0404@Gmail.com>
2016-02-01 12:55:33 -0800

    add wikiSearch.service

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -6,6 +6,7 @@ import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
+import { WikiSearch } from '../app/components/wikiSearch/wikiSearch.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
@@ -17,6 +18,7 @@ angular.module('ngtest', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ng
   .run(runBlock)
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
+  .service('wikiSearch', WikiSearch)
   .controller('MainController', MainController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
